625af1516dd8b352f0aace956a3a5e0dae3d81e8
codingwithomansaakib <omansaakib@gmail.com>
2026-07-10 22:39:51 +0530

    Update API URL for production

--- frontend/config.js
+++ frontend/config.js
@@ -2,4 +2,4 @@ const API_URL =
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:3000"
-        : "https://YOUR-RAILWAY-URL.up.railway.app";
+        : "https://nearbyfix-backend-2dm4.onrender.com";
